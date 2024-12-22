457e7e658a76c3daf001853d9eee7d27f24d3590
Ichiro Yamazaki <16699009+ichiroyamazaki@users.noreply.github.com>
2024-12-22 23:40:11 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+num1 = int (num1)
+num2 = int (num2)
+if numi ‹ num2:
+  print("The first number is less than the second number.")
+elif numi › num2:
+  print("The first number is greater than the second number.")
+else:
+  print("The first number is equal to the second number.")
